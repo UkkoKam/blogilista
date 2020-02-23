4403980be4fca06c13607003c24028cbc6b952c1
Ukko Kamula <kamulaukko@gmail.com>
2020-02-23 19:53:26 +0200

    4.19 done

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 
@@ -24,5 +25,6 @@ app.use(bodyParser.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response, next) => {
   try {
@@ -16,30 +25,31 @@ blogsRouter.get('/', async (request, response, next) => {
 blogsRouter.post('/',  async (request, response) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
-  
-  const blog = new Blog({
+  const token = getTokenFrom(request)
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if(!token || !decodedToken.id) {
+      return response.status(401).json({error: 'token missing or'})
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes ? body.likes : 0,
       user: user.id
   })
-
-  if (blog.title && blog.url) {
-    try {
-      const savedBlog = await blog.save()
-      user.blogs = user.blogs.concat(savedBlog._id)
-      await user.save()
-      response.json(savedBlog.toJSON())
-    } catch(exception) {
-      next(exception)
-    }
-  } else {
-    return response.status(400).json({ error: 'url and title must be present'})
-  }
-
   
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  response.json(savedBlog.toJSON())
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
@@ -47,7 +57,7 @@ blogsRouter.delete('/:id', async (request, response) => {
       await Note.findByIdAndRemove(request.params.id)
       response.status(204).end()
   } catch(exception) {
-      next(exception)
+      console.log(exception)
   }
 })
 
